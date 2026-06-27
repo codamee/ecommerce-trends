b5d7510634a3292f83d92f5fddff37868e3a8ca8
naveen <185893220+codamee@users.noreply.github.com>
2026-06-27 13:40:04 +0530

    style : add the scrollbar-gutter:stable

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div className="container  m-auto">
       <div className="flex py-20 px-10 gap-6 justify-between">
-        <div className="flex w-1/2 flex-col gap-10 items-start">
+        <div className="flex w-1/2 flex-col gap-10 items-start  justify-end">
           <h1 className="text-5xl font-bold text-green-700">Clothes That Gets You Noticed</h1>
           <p className="text-[1.2rem] ">Fashion is part of the daily air and it does not quite help that it changes all the time. Clothes have always been a marker of the era and we are in a revolution. Your fashion makes you been seen and heard that way you are. So, celebrate the seasons new and exciting fashion in your own way.
           </p>
